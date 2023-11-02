62fecb3e9654a35a08357b0abab866c161dd02ea
abrahamjacob98 <abejacs@gmail.com>
2023-11-02 11:19:19 +0000

    Adding continuous integration via python-tests.yml

--- times.py
+++ times.py
@@ -4,11 +4,28 @@ import datetime
 def time_range(start_time, end_time, number_of_intervals=1, gap_between_intervals_s=0):
     start_time_s = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end_time_s = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
-    d = (end_time_s - start_time_s).total_seconds() / number_of_intervals + gap_between_intervals_s * (1 / number_of_intervals - 1)
-    sec_range = [(start_time_s + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
-                  start_time_s + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s))
-                 for i in range(number_of_intervals)]
-    return [(ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S")) for ta, tb in sec_range]
+    if start_time_s > end_time_s:
+        raise ValueError("The start time is greater than the end time! The times are reversed, please ammend this.")
+    
+    d = (
+        end_time_s - start_time_s
+    ).total_seconds() / number_of_intervals + gap_between_intervals_s * (
+        1 / number_of_intervals - 1
+    )
+    sec_range = [
+        (
+            start_time_s
+            + datetime.timedelta(seconds=i * d + i * gap_between_intervals_s),
+            start_time_s
+            + datetime.timedelta(seconds=(i + 1) * d + i * gap_between_intervals_s),
+        )
+        for i in range(number_of_intervals)
+    ]
+    return [
+        (ta.strftime("%Y-%m-%d %H:%M:%S"), tb.strftime("%Y-%m-%d %H:%M:%S"))
+        for ta, tb in sec_range
+    ]
+
 
 
 def compute_overlap_time(range1, range2):
@@ -19,6 +36,7 @@ def compute_overlap_time(range1, range2):
             high = min(end1, end2)
             overlap_time.append((low, high))
     return overlap_time
+
 
 if __name__ == "__main__":
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
--- times_test.py
+++ times_test.py
@@ -1,14 +1,42 @@
-from times  import compute_overlap_time
+from times import compute_overlap_time
 from times import time_range
 import pytest
 
-def test_given_input():
+# from times import
 
-    if __name__ == "__main__":
-        large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-        short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-        result = compute_overlap_time(large,short)
-        expected = (1.75*60*60)
-        print(compute_overlap_time(large, short))
 
-pytest
+# def test_given_input():
+#     if __name__ == "__main__":
+#         large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+#         short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+#         result = compute_overlap_time(large, short)
+#         expected = 1.75 * 60 * 60
+#         assert result == expected
+#         print(compute_overlap_time(large, short))
+
+
+# def test_overlap():
+#     large = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+#     short = time_range("2010-01-12 11:30:00", "2010-01-12 11:45:00", 2, 60)
+#     result = compute_overlap_time(large, short)
+#     expected = 0
+#     assert result == expected
+#     print(compute_overlap_time(large, short))
+
+
+def test_edge_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00")
+    short = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00", 2, 60)
+    result = compute_overlap_time(large, short)
+    expected = 0
+    print(compute_overlap_time(large, short))
+    assert result == expected
+
+
+# def test_reverse_time():
+#     large = time_range("2010-01-12 11:00:00", "2010-01-12 10:00:00")
+#     # short = time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00", 2, 60)
+#     result = large
+#     expected = True
+#     assert result == expected
+#     # print(compute_overlap_time(large, short))
